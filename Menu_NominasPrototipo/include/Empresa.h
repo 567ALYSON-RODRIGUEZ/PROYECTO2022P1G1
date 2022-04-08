8f3156c1e08ef739dc3659088e144d5afd8eb345
Alyson Rodríguez <arodriguezq3@miumg.edu.gt>
2022-04-07 18:51:19 -0600

    creacion atributos y metodos de clase Empresa

--- Menu_NominasPrototipo/include/Empresa.h
+++ Menu_NominasPrototipo/include/Empresa.h
@@ -1,16 +1,25 @@
+#include<cstring>
 #ifndef EMPRESA_H
 #define EMPRESA_H
+using namespace std;
 
 
 class Empresa
 {
-    public:
-        Empresa();
-        virtual ~Empresa();
-
-    protected:
+public:
+    display();
+    insertE();
+    ingresarE();
+    menu();
+    modify();
+    searchE();
+    deletE();
 
     private:
+        string nameE,mail,nameR,ecoActivity;
+        long int nit,address,phone;
+        int staffNumber, companyNumber;
 };
+
 
 #endif // EMPRESA_H
--- Menu_NominasPrototipo/src/Empresa.cpp
+++ Menu_NominasPrototipo/src/Empresa.cpp
@@ -1,4 +1,11 @@
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+#include<iostream>
+#include<fstream>
 #include "Empresa.h"
+using namespace std;
 
 Empresa::Empresa()
 {
@@ -9,3 +16,4 @@ Empresa::~Empresa()
 {
     //dtor
 }
+
